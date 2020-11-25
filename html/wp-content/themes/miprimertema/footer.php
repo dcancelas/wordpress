afb1c0543d38711038472956fd2e07db6026ce9e
dcancelas <dcancelasgarcia@danielcastelao.org>
2020-11-25 11:49:55 +0100

    Cambios en miprimertema

--- html/wp-content/themes/miprimertema/footer.php
+++ html/wp-content/themes/miprimertema/footer.php
@@ -1,2 +1,8 @@
-<?php
-echo "<h1> FOOTER </h1>";
+<!DOCTYPE html>
+<html>
+    <body>
+        <div id="footer">
+            <p>Tema creado por <a href="https://github.com/dcancelas">dcancelas</a></p>
+        </div>
+    </body>
+</html>
--- html/wp-content/themes/miprimertema/header.php
+++ html/wp-content/themes/miprimertema/header.php
@@ -1,2 +1,15 @@
-<?php
-echo "<h1> HEADER </h1>";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div id="header">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/gintoki.jpeg">
+            <h1 id="name"><?php bloginfo( 'name' ); ?></h1>
+            <h2><?php bloginfo( 'description' ); ?></h2>
+        </div>
+    </body>
+</html>
